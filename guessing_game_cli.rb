39888933da7665ee47c2d5c0e5dff897932c9ce3
erin johnson <erin.nicole.johnson@gmail.com>
2019-06-30 10:12:09 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,1 +1,15 @@
 # Code your solution here!
+
+def run_guessing_game
+  message = ""
+  compnum = rand(6) + 1
+  input = gets.chomp
+
+  input == "exit" ? message = "Goodbye!" : message = "You guessed the correct number!"
+
+  while input != compnum && input != "exit"
+    message = "Sorry! The computer guessed #{compnum}"
+  end
+
+p message
+end
