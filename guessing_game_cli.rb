454d1cf6bfba35847bd1778af644b49e11a7dc60
erin johnson <erin.nicole.johnson@gmail.com>
2019-06-30 10:41:44 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -7,7 +7,7 @@ def run_guessing_game
 
   if input == "exit"
     message = "Goodbye!"
-  elsif input == compnum #&& input != "exit"
+  elsif input == "exit" #&& input != "exit"
     message = "You guessed the correct number!"
   else
     message = "Sorry! The computer guessed #{compnum}."
